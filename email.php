696d2765c075d147601e408c00c0963532cb9700
sheikh hussnain <sheikhhussnain23@gmail.com>
2018-01-25 01:50:57 +0500

    Email sending

--- email.php
+++ email.php
@@ -1,5 +1,17 @@
 <?php 
-//$email = $_POST['email'];
-header('Access-Control-Allow-Origin: *');
-echo json_encode('Thanks for subscription!');
+header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
+if (isset($_POST['email'])) {
+
+	$email = $_POST['email'];
+	$headers = 'From: <sheikhhussnain23@gmail.com>' . "\r\n";
+	if(mail($email, "hussnainsheikh.github.io", "Thanks for Subcribing. Stay in touch", $headers)){
+		echo json_encode(array('error'=> false, 'data' => 'Thanks for subscription!', 'email' => $email));
+	}
+	$headers = 'From: New Subscriber<'.$email.'>';
+	mail('sheikhhussnain23@gmail.com', 'New Subscriber', 'There is a new subscriber on your website: '. $email);
+}
+
 ?>
